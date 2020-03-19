67ad1ae22e59d7de6eb815e8a92e0d3ee68422ab
Naoto <nnomura98@gmail.com>
2020-03-20 00:56:49 +0100

    Simple root component template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-    
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Superhero search!</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"/>
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+                <b-button href="https://github.com/NaotoNomura/superherosearch" size="sm" type="submit">GitHub</b-button>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+
+        <b-row>
+            <b-col><!-- Search Component --></b-col>
+        </b-row>
+    </div>
 </template>
 
 <script lang="ts">
