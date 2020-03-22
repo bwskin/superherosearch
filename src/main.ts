513949a78cc12a99dd22fce5b620964bf3fb433a
Naoto <nnomura98@gmail.com>
2020-03-22 19:38:03 +0100

    Bootstrap cherrypicking for smaller bundle

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
 import App from './App.vue'
+import { BCol, BRow, BTableSimple, BThead, BTh, BTr, BTd, BButton, BFormInput, BForm, BTbody, BCollapse } from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.component('b-col', BCol)
+Vue.component('b-row', BRow)
+Vue.component('b-table-simple', BTableSimple)
+Vue.component('b-thead', BThead)
+Vue.component('b-th', BTh)
+Vue.component('b-tr', BTr)
+Vue.component('b-td', BTd)
+Vue.component('b-button', BButton)
+Vue.component('b-form-input', BFormInput)
+Vue.component('b-form', BForm)
+Vue.component('b-tbody', BTbody)
+Vue.component('b-collapse', BCollapse)
 
 new Vue({
     el: "#app",
     components: {
-        App
+        App, 
     },
     template: "<App/>"
 });
