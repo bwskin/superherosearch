1cd243573c9339e5a6f39ae70205995cbe66e40e
Naoto <nnomura98@gmail.com>
2020-03-22 19:40:47 +0100

    Adding mocks

---
+++ tests/mocks.js
@@ -0,0 +1,47 @@
+export default {
+    searchResults: [
+        {
+            name: "HERO1",
+            biography: { 
+                publisher:"PUB1"
+            }
+        },
+        {
+            name: "HERO2",
+            biography: {
+                publisher:"PUB2"
+            }
+        },
+        {
+            name: "HERO3",
+            biography: {
+                publisher:"PUB3"
+            }
+        }
+    ],
+
+    profile: {
+        name: "HERO1",
+        biography: {
+            publisher: "PUB1",
+            "full-name": "So brave hero",
+            "place-of-birth": "Nobody knows"
+        },
+        appearance: {
+            gender: "Manly manly man",
+            height: ["So tall!", "Even taller!"],
+            race: "He's unique"
+        },
+        powerstats: {
+            intelligence: 100,
+            strength: 100,
+            speed: 100,
+            durability: 100,
+            power: 100,
+            combat: 100
+        },
+        image: {
+            url: "some_url"
+        }
+    }
+}
