2cf7153e9979498bc26a6da56504f1fbc928183f
Naoto <nnomura98@gmail.com>
2020-03-22 19:40:26 +0100

    fixing api-service (node bug?)

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -1,9 +1,11 @@
 export default class ApiService {
 
     public static async search(name: string): Promise<Array<object>> {
-        const response = await fetch("/api/2645248895760055/search/"+name);
-        const jsonResponse = await response.json();
-        return jsonResponse.results
+        return (
+            await (
+                await fetch("/api/2645248895760055/search/"+name)
+            ).json()
+        ).results;
     }
 
 }
